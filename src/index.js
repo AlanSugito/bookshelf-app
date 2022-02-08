9ac49c97f5f195ce970397e91e65f122f00f39a9
Alan Sugito <alansugito75@gmail.com>
2022-02-08 09:44:19 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const submit = document.querySelector(".add-button");
   const form = document.querySelector("form");
-  const read = document.querySelector(".read-book")
-  const completed = document.querySelector(".completed-book")
 
   submit.addEventListener("click", (e) => {
     e.preventDefault();
     addBook();
   });
-  form.addEventListener("submit", e => {
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
     addBook();
   });
-
-  read.addEventListener("input", e => {
-    console.log(e)
-  })
-  
-  
 });
-
 
 loadBook();
